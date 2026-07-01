d7afb46c55628d404b210075ff98f969ce053592
vinodbankey <vinod.bankey2015@gmail.com>
2026-07-02 00:52:49 +0530

    TF

--- implicit.tf
+++ implicit.tf
@@ -15,17 +15,3 @@ resource "azurerm_resource_group" "mytestrg" {
   name     = "test-rg"
   location = "West us"
 }
-
-resource "azurerm_storage_account" "mymoneygodam" {
-  name                     = "maalgodam"
-  resource_group_name      = azurerm_resource_group.mytestrg.name
-  location                 = azurerm_resource_group.mytestrg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "maaldacontainer" {
-  name                  = "containetmaalda"
-  storage_account_id    = azurerm_storage_account.mymoneygodam.id
-  container_access_type = "private"
-}
